478f005506bd655fa90c4a95c686259877f34d22
Matheus <matheus_rock14@hotmail.com>
2019-07-09 21:19:27 -0300

    Initial Commit

---
+++ mobile-app-ws/src/main/java/com/projects/mobileappws/SpringCourse.java
@@ -0,0 +1,13 @@
+package com.projects.mobileappws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourse.class, args);
+	}
+
+}
---
+++ mobile-app-ws/src/test/java/com/projects/mobileappws/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.mobileappws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
